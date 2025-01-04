f00e2c6ff6b4768265e44efe14c768994004edb7
Harshal Dongare <myeducationalarena@gmail.com>
2025-01-04 20:17:19 +0530

    [exercise] upload solution to exercise 06

---
+++ exercise_06_airline_database.sql
@@ -0,0 +1,56 @@
+
+-- ----------------------------------------------------------
+-- 						Clauses and Alias					|
+-- ----------------------------------------------------------
+
+-- Create the airline database if it does not already exist
+CREATE DATABASE IF NOT EXISTS airline1234;
+
+-- Switch to airline1234 database
+USE airline1234;
+
+-- ----------------------------------
+-- 			Table 1: FLIGHTS		|
+-- ----------------------------------
+
+-- Create FLIGHTS table
+CREATE TABLE flights(
+	flight_id INT PRIMARY KEY,									-- Unique identifier for each flight (Primary Key)
+    flight_number VARCHAR(10) NOT NULL,							-- Flight Number, cannot be NULL
+    departure_airport VARCHAR(100) NOT NULL,					-- Name of the departure airport, cannot be NULL
+    arrival_airport VARCHAR(100) NOT NULL,						-- Name of the arrival airport, cannot be NULL
+    departure_time DATETIME NOT NULL,							-- Schedule departure time, cannot be NULL
+    arrival_time DATETIME NOT NULL,								-- Schedule arrival time, cannot be NULL
+    flight_duration INT NOT NULL,								-- Duration of the flight in minutes, cannot be NULL
+    seats_available INT NOT NULL CHECK (seats_available >= 0), 	-- Number of available seats, must be postive and cannot be NULL
+    aircraft_type VARCHAR(50) NOT NULL,							-- Type of the aircraft used for flight, cannot be NULL
+    status ENUM('On Time', 'Delayed', 'Cancelled') 				
+		NOT NULL DEFAULT 'On Time'								-- Current status of the flight, cannot be NULL and DEFAULT status is 'On Time'
+);
+
+-- Insert records into FLIGHTS table
+INSERT INTO flights(flight_id, flight_number, departure_airport, arrival_airport, departure_time, arrival_time, flight_duration, seats_available, aircraft_type, status)
+VALUES(1, 'AI101', 'Delhi (DEL)', 'Mumbai (BOM)', '2023-10-01 06:00:00', '2023-10-01 08:00:00', 120, 50, 'Boeing 737', 'On Time'),
+(2, 'AI102', 'Mumbai (BOM)', 'Delhi (DEL)', '2023-10-01 09:00:00', '2023-10-01 11:00:00', 120, 60, 'Airbus A320', 'On Time'),
+(3, 'AI103', 'Delhi (DEL)', 'Bangalore (BLR)', '2023-10-01 07:00:00', '2023-10-01 09:30:00', 150, 70, 'Boeing 737', 'On Time'),
+(4, 'AI104', 'Bangalore (BLR)', 'Delhi (DEL)', '2023-10-01 10:00:00', '2023-10-01 12:30:00', 150, 80, 'Airbus A320', 'On Time'),
+(5, 'AI105', 'Delhi (DEL)', 'Chennai (MAA)', '2023-10-01 08:00:00', '2023-10-01 10:30:00', 150, 40, 'Boeing 737', 'On Time'),
+(6, 'AI106', 'Chennai (MAA)', 'Delhi (DEL)', '2023-10-01 11:00:00', '2023-10-01 13:30:00', 150, 50, 'Airbus A320', 'On Time'),
+(7, 'AI107', 'Delhi (DEL)', 'Kolkata (CCU)', '2023-10-01 09:00:00', '2023-10-01 11:30:00', 150, 30, 'Boeing 737', 'On Time'),
+(8, 'AI108', 'Kolkata (CCU)', 'Delhi (DEL)', '2023-10-01 12:00:00', '2023-10-01 14:30:00', 150, 20, 'Airbus A320', 'On Time'),
+(9, 'AI109', 'Delhi (DEL)', 'Hyderabad (HYD)', '2023-10-01 10:00:00', '2023-10-01 12:30:00', 150, 60, 'Boeing 737', 'On Time'),
+(10, 'AI110', 'Hyderabad (HYD)', 'Delhi (DEL)', '2023-10-01 13:00:00', '2023-10-01 15:30:00', 150, 50, 'Airbus A320', 'On Time'),
+(11, 'AI111', 'Delhi (DEL)', 'Pune (PNQ)', '2023-10-01 14:00:00', '2023-10-01 16:00:00', 120, 40, 'Boeing 737', 'On Time'),
+(12, 'AI112', 'Pune (PNQ)', 'Delhi (DEL)', '2023-10-01 17:00:00', '2023-10-01 19:00:00', 120, 30, 'Airbus A320', 'On Time'),
+(13, 'AI113', 'Delhi (DEL)', 'Ahmedabad (AMD)', '2023-10-01 15:00:00', '2023-10-01 17:00:00', 120, 50, 'Boeing 737', 'On Time'),
+(14, 'AI114', 'Ahmedabad (AMD)', 'Delhi (DEL)', '2023-10-01 18:00:00', '2023-10-01 20:00:00', 120, 40, 'Airbus A320', 'On Time'),
+(15, 'AI115', 'Delhi (DEL)', 'Jaipur (JAI)', '2023-10-01 16:00:00', '2023-10-01 17:30:00', 90,  60, 'Boeing 737', 'On Time');
+
+
+-- --------------------------------------------------------------
+-- 						SELECT Query 							|
+-- --------------------------------------------------------------
+
+-- SELECT all records from FLIGHTS table
+SELECT * FROM flights;
+
